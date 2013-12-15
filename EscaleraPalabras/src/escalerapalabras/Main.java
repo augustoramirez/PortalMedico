f3341a5577cf8848d2014c96e7e0a6700363c6bf
augustoramirez <augusto.ramirez@gmail.com>
2013-12-14 20:28:43 -0600

    x
    c

--- EscaleraPalabras/src/escalerapalabras/Main.java
+++
@@ -1,8 +0,0 @@
-package escalerapalabras;
-
-public class Main {
-    public static void main(String[] args) {
-        Principal principal = new Principal();        
-        principal.setVisible(true);        
-    }
-}
